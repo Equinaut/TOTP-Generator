b42ecb3e7a5b325b49c7d34e967cc1557b7f7399
James Cameron <git@jamescameron.me>
2024-08-09 21:33:41 +0100

    Implement function to generate TOTP codes

--- totp.py
+++ totp.py
@@ -33,3 +33,31 @@ def HMAC_SHA1(key, message):
     # Calculate the digest of k0_opad_hash1 to produce the final HMAC value
     hmac = hashlib.sha1(k0_opad_hash1).digest()
     return hmac
+
+def generate_code(key, time, digits = 6, period = 30):
+    # Generate a TOTP code for a given key and time stamp
+    # Defaults to 6 digit codes that last 30 seconds
+
+    # Create 8 byte counter value
+    counter = bytearray([((time // period) >> 8 * i) & 0xff for i in range(7, -1, -1)])
+
+    # Calculate HMAC value
+    hmac = HMAC_SHA1(key, counter)
+
+    # Calculate TOTP code from the HMAC value
+    offsetBits = hmac[19] & 0xf
+    vals = "".join(bin(i)[2:].zfill(8) for i in hmac[offsetBits : offsetBits + 4])
+    return str(int(vals[1:], base = 2) % 10 ** digits).zfill(digits)
+
+def convert_secret(secret):
+    # Converts a base 32 encoded secret into a byte array
+    values = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+
+    # Convert base 32 digits to binary
+    binaryValues = [bin(values.index(c))[2:].zfill(5) for c in secret]
+    joined = "".join(binaryValues)
+    
+    # Split binary digits into bytes
+    bytes = [int(joined[i: i + 8], base = 2) for i in range(0, len(joined), 8)]
+    
+    return bytearray(bytes)
